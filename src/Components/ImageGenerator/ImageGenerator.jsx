1e77901062ccb1b9617f3b23875880d0b4ee7c94
Nitya <niittss27@gmail.com>
2024-06-22 11:29:18 +0530

    Project completed

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -7,6 +7,37 @@ const ImageGenerator = () => {
 const [image_url,setImage_url] = useState("/")
 let inputRef = useRef(null)
 
+const [loading, setLoading] = useState(false)
+
+const imageGenerator = async () => {
+    if (inputRef.current.value==="") {
+        return 0;
+    }
+    setLoading(true)
+    const response = await fetch(
+        "https://api.openai.com/v1/images/generations",
+        {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization:
+                "YOUR_API_KEY",
+                "User-Agent":"Chrome"
+            },
+            body:JSON.stringify({
+                prompt: `${inputRef.current.value}`,
+                n:1,
+                size:"512x512"
+            }),
+
+        }
+    );
+
+    let data = await response.json();
+   let data_array = data.data;
+   setImage_url(data_array[0].url)
+setLoading(fa)
+}
 
   return (
     <div className="ai-image-generator">
@@ -16,11 +47,16 @@ let inputRef = useRef(null)
       <div className="img-loading">
         <div className="image">
           <img src={image_url==="/"?default_image:image_url} alt="" />
+          <div className="loading">
+    <div className={loading?"loading-bar-full":"loading-bar"}>
+    </div>
+    <div className={loading?"loading-text":"display-none"}>Loading...</div>
+          </div>
         </div>
       </div>
       <div className="search-box">
-        <input type="text" className="search-input" placeholder="Describe what you want to see?"/>
-        <div className="generate-btn">
+        <input type="text" ref={inputRef} className="search-input" placeholder="Describe what you want to see?"/>
+        <div className="generate-btn" onClick={() => {imageGenerator()}}>
             Generate
         </div>
       </div>
